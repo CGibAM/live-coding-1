b35726ed5df12306289a8adcd4645f229a840ae6
Cristian Gibran <magaaa511@gmail.com>
2023-06-22 15:44:48 -0600

    Se añade la resolucion del ejercicio de livecoding

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 // Escribe tu código aquí.
+const lista= document.getElementById("myList");
+const inputTexto = document.getElementById("comparar");
+const inputArray = document.getElementById("inputLits");
+const boton = document.getElementById("boton");
+
+var array2=[];
+
+function comparar(){
+    var texto = inputTexto.value;
+    var array = inputArray.value;
+
+    var arrayCorregido = array.split(",");
+
+    arrayCorregido.forEach(e => {
+        if(e.length>texto.length){
+            array2.push(e);
+        }
+    });
+
+    console.log(array2);
+
+}
+
+function pintarDom(){
+    comparar();
+    array2.forEach(e => {
+        const list = document.createElement("li");
+        list.innerHTML = `${e}`;
+        lista.appendChild(list);
+    });
+}
+
+
+boton.addEventListener("click",pintarDom);
